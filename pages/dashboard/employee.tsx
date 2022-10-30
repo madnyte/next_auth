bc0ac272f5a108f1dbaf514cf02c688175f16878
motheo <motheo.ken@gmail.com>
2022-10-30 17:08:56 +0200

    fix dashboard reading undefined values

--- pages/dashboard/employee.tsx
+++ pages/dashboard/employee.tsx
@@ -22,7 +22,7 @@ const EmployeeDashboard = ({user}: EmployeeProps) => {
 		<>
 				<div className="flex flex-col justify-center gap-3 sm:gap-4 lg:gap-8">
 					<h4 className="text-xl sm:text-3xl">You are logged in</h4>
-					<h2 className="text-xl sm:text-3xl">Email: {user.email}</h2>
+					<h2 className="text-xl sm:text-3xl">Email: {user?.email}</h2>
 					<button
 						onClick={onSubmit}
 						className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
--- pages/dashboard/user.tsx
+++ pages/dashboard/user.tsx
@@ -23,7 +23,7 @@ const UserDashboard = ({user}: UserProps) => {
 		<>
 				<div className="flex flex-col justify-center gap-3 sm:gap-4 lg:gap-8">
 					<h4 className="text-xl sm:text-3xl">You are logged in</h4>
-					<h2 className="text-xl sm:text-3xl">Phone Number: {user.phoneNumber}</h2>
+					<h2 className="text-xl sm:text-3xl">Phone Number: {user?.phoneNumber}</h2>
 					<button
 						onClick={onSubmit}
 						className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
